865ff38c698bbf4c769459808ddab58806995c6b
Shayan Mazahir <shayanmazahir@gmail.com>
2025-12-23 18:55:15 -0500

    restarting the way I learn

--- main.rs
+++
@@ -1,7 +0,0 @@
-//exploring commands, sorta part of the tutorial ig?
-fn main() {
-    println!("ME IS ALIVEEEEEEE!");
-
-    //wait for 3 seconds
-    std::thread::sleep(std::time::Duration::from_secs(3));
-}
